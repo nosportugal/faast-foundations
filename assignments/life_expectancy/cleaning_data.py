b992f9803314f054ee6fc660481d5f810e4a9eb0
João Carlos Pires da Silva (CAA) <joao.pires.silva@fidelidade.pt>
2023-05-05 16:14:10 +0100

    Assigmnment2

--- assignments/life_expectancy/cleaning_data.py
+++ assignments/life_expectancy/cleaning_data.py
@@ -9,21 +9,33 @@ import numpy as np
 AUX_NAME_INFILE = "eu_life_expectancy_raw.tsv"
 AUX_NAME_OUTPUT= "pt_life_expectancy.csv"
 AUX_DATA_PATH = Path(__file__).parent / 'data'
-AUX="JOAO"
-#funcao clifeandata
 
-def clean_data(
-    country="PT",
+#funcao load_data
+
+def load_data(
     file_input=AUX_NAME_INFILE,
-    file_output=AUX_NAME_OUTPUT,
     file_path=AUX_DATA_PATH
     ):
 
-    """Funcao:clifean_data para limpeza de ficheiro"""
+    """Funcao:load_data para leitura de ficheiro"""
 
     print(f"Abre ficheiro: {file_input}")
-    print(f"Arg:{country}")
+    
     life = pd.read_csv(os.path.join(file_path,file_input), delimiter='\t')
+    return life
+   
+
+
+
+#funcao clean_data
+
+def clean_data(
+    life,
+    country="PT",
+    ):
+
+    """Funcao:clean_data para limpeza de dados"""
+
     print("Alteracoes em curso...")
     life[['unit','sex','age','region']] = life['unit,sex,age,geo\\time'].str.split(",",expand=True)
     life.drop(columns='unit,sex,age,geo\\time', inplace=True)
@@ -33,9 +45,20 @@ def clean_data(
     life = life.dropna(axis=0, how='any',subset='value')
     life['value'] = life['value'].str.split(' ').str[0]
     life = life.astype({'value':float}, errors="raise")
-    life_pt = life[life["region"] == country]
-    print(f"Vai gerar o ficheiro: {file_output}")
-    life_pt.to_csv(os.path.join(file_path,file_output), index=False, encoding='utf-8')
+    life = life[life["region"] == country]
+    return life
+
+def save_data(
+    file_input=AUX_NAME_INFILE,
+    file_path=AUX_DATA_PATH
+    ):
+
+    """Funcao:load_data para leitura de ficheiro"""
+
+    print(f"Abre ficheiro: {file_input}")
+    
+    life = pd.read_csv(os.path.join(file_path,file_input), delimiter='\t')
+    return life
 
 if __name__ == "__main__" :
 
