c7618a646aa328f43a611e11c3771afd14e1cbcd
Luis Filipe Rebelo da Costa (CAA) <luis.rebelo.costa@fidelidade.pt>
2023-05-05 16:30:40 +0100

    split em 3 funções

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -11,23 +11,48 @@ FILE_AUX_INPUT = "eu_life_expectancy_raw.tsv"
 FILE_AUX_OUTPUT = "pt_life_expectancy.csv"
 DATA_PATH = Path(__file__).parent / 'data'
 
-def clean_data(
-    fname_input=FILE_AUX_INPUT,
-    fname_output=FILE_AUX_OUTPUT,
+
+def load_data(fname_input=FILE_AUX_INPUT):
+
+    """Função que importa ficheiro """
+    life = pd.read_csv(os.path.join(DATA_PATH, fname_input), delimiter='\t')
+    return life
+
+def clean_data(df_input,
     country= "PT"):
 
-    """Função que importa ficheiro e trabalha dados e exporta novamente"""
-    life = pd.read_csv(os.path.join(DATA_PATH, fname_input), delimiter='\t')
-    life[['unit','sex','age','region']]=life['unit,sex,age,geo\\time'].str.split(",",expand=True)
-    life.drop(columns='unit,sex,age,geo\\time', inplace=True)
-    life = pd.melt(life, id_vars=['unit', 'sex', 'age', 'region'], var_name="year")
-    life = life.astype({'year':'int'})
-    life.replace(": ", np.nan, inplace  = True)
-    life = life.dropna(axis=0, how='any', subset="value")
-    life['value'] = life['value'].str.split(' ').str[0]
-    life = life.astype({'value':float}, errors="raise")
-    life_pt = life[life["region"] == country]
-    life_pt.to_csv(os.path.join(DATA_PATH, fname_output), index=False, encoding='utf-8')
+    """Função que trabalha dados """
+    df_input[['unit','sex','age','region']]=(
+        df_input['unit,sex,age,geo\\time'].str.split(",",expand=True)
+    )
+    df_input.drop(columns='unit,sex,age,geo\\time', inplace=True)
+    df_input = pd.melt(df_input, id_vars=['unit', 'sex', 'age', 'region'], var_name="year")
+    df_input = df_input.astype({'year':'int'})
+    df_input.replace(": ", np.nan, inplace  = True)
+    df_input = df_input.dropna(axis=0, how='any', subset="value")
+    df_input['value'] = df_input['value'].str.split(' ').str[0]
+    df_input = df_input.astype({'value':float}, errors="raise")
+    df_input = df_input[df_input["region"] == country]
+    return df_input
+
+def save_data(df_input, fname_output=FILE_AUX_OUTPUT):
+
+    """Função que exporta ficheiro"""
+    df_input.to_csv(os.path.join(DATA_PATH, fname_output), index=False, encoding='utf-8')
+
+def main(fname_input=FILE_AUX_INPUT,
+    country="PT",
+    fname_output=FILE_AUX_OUTPUT):
+
+    """Função main que invoca as 3 funções """
+    df_load = load_data(fname_input)
+    df_load = clean_data(df_load, country)
+    save_data(df_load, fname_output)
+
+
+
+
+
 
 if __name__ == "__main__":  # pragma: no cover
     parser = ag.ArgumentParser(description='Indicar o Pais.')
@@ -35,4 +60,5 @@ if __name__ == "__main__":  # pragma: no cover
     args = parser.parse_args()
 
     #clean_data("eu_life_expectancy_raw.tsv", "pt_life_expectancy_teste.csv",args.country )
-    clean_data(country=args.country )
+    #clean_data(country=args.country )
+    main(country=args.country)
--- assignments/life_expectancy/tests/test_cleaning.py
+++ assignments/life_expectancy/tests/test_cleaning.py
@@ -1,13 +1,13 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import main
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data()
+    main()
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
