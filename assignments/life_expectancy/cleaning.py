da95435bb3acdf140008517549a6b4efb77f8491
João Carlos Pires da Silva (CAA) <joao.pires.silva@fidelidade.pt>
2023-05-05 15:42:22 +0100

    assignment 1

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,4 +1,6 @@
 """Modulo de Limpeza de Ficheiro"""
+import os
+from pathlib import Path
 import argparse as ag
 import pandas as pd
 import numpy as np
@@ -6,19 +8,21 @@ import numpy as np
 #variaveris auxiliares
 AUX_NAME_INFILE = "eu_life_expectancy_raw.tsv"
 AUX_NAME_OUTPUT= "pt_life_expectancy.csv"
+AUX_DATA_PATH = Path(__file__).parent / 'data'
 #funcao clifeandata
 
 def clean_data(
     country="PT",
     file_input=AUX_NAME_INFILE,
-    file_output=AUX_NAME_OUTPUT
+    file_output=AUX_NAME_OUTPUT,
+    file_path=AUX_DATA_PATH
     ):
 
     """Funcao:clifean_data para limpeza de ficheiro"""
 
     print(f"Abre ficheiro: {file_input}")
     print(f"Arg:{country}")
-    life = pd.read_csv("data/" + file_input, delimiter='\t')
+    life = pd.read_csv(os.path.join(file_path,file_input), delimiter='\t')
     print("Alteracoes em curso...")
     life[['unit','sex','age','region']] = life['unit,sex,age,geo\\time'].str.split(",",expand=True)
     life.drop(columns='unit,sex,age,geo\\time', inplace=True)
@@ -30,7 +34,7 @@ def clean_data(
     life = life.astype({'value':float}, errors="raise")
     life_pt = life[life["region"] == country]
     print(f"Vai gerar o ficheiro: {file_output}")
-    life_pt.to_csv("data/" + file_output, index=False, encoding='utf-8')
+    life_pt.to_csv(os.path.join(file_path,file_output), index=False, encoding='utf-8')
 
 if __name__ == "__main__" :
 
