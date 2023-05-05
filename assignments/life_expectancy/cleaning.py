f366a2b65b1655affd6fda5b72274e49214f9616
Luis Filipe Rebelo da Costa (CAA) <luis.rebelo.costa@fidelidade.pt>
2023-05-05 15:30:15 +0100

    'alteração'

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,10 +1,15 @@
 """Specifica o que este script faz"""
 import argparse as ag
+from pathlib import Path
+import os
 import pandas as pd
 import numpy as np
 
+
+
 FILE_AUX_INPUT = "eu_life_expectancy_raw.tsv"
 FILE_AUX_OUTPUT = "pt_life_expectancy.csv"
+DATA_PATH = Path(__file__).parent / 'data'
 
 def clean_data(
     fname_input=FILE_AUX_INPUT,
@@ -12,7 +17,7 @@ def clean_data(
     country= "PT"):
 
     """Função que importa ficheiro e trabalha dados e exporta novamente"""
-    life = pd.read_csv("data/" + fname_input, delimiter='\t')
+    life = pd.read_csv(os.path.join(DATA_PATH, fname_input), delimiter='\t')
     life[['unit','sex','age','region']]=life['unit,sex,age,geo\\time'].str.split(",",expand=True)
     life.drop(columns='unit,sex,age,geo\\time', inplace=True)
     life = pd.melt(life, id_vars=['unit', 'sex', 'age', 'region'], var_name="year")
@@ -22,7 +27,7 @@ def clean_data(
     life['value'] = life['value'].str.split(' ').str[0]
     life = life.astype({'value':float}, errors="raise")
     life_pt = life[life["region"] == country]
-    life_pt.to_csv("data/" + fname_output, index=False, encoding='utf-8')
+    life_pt.to_csv(os.path.join(DATA_PATH, fname_output), index=False, encoding='utf-8')
 
 if __name__ == "__main__":  # pragma: no cover
     parser = ag.ArgumentParser(description='Indicar o Pais.')
