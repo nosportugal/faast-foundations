5c139aaed451731ac48febab30a395ed19eb943e
Joaquim Leitao <jpleitao@corporativo.pt>
2024-01-02 12:05:33 +0000

    Assignment 3 modifications: Main function return DataFrame, separate into distinct modules

---
+++ assignments/life_expectancy/defaults.py
@@ -0,0 +1,12 @@
+"""Module with defaults and constants"""
+# coding: utf-8
+
+
+# Defining default values for the module's arguments
+COUNTRY_ARG_STR = "country"
+INPUT_FILE_PATH_ARG_STR = "input_path"
+OUTPUT_FILE_PATH_ARG_STR = "output_path"
+
+# Defining constants for column names and tsv column separator
+TABLE_KEY_VARS = ["unit", "sex", "age", "region"]
+DEFAULT_FILE_SEP = "[\t,]"
